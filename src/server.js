9a1510f3b2720799898dd8be8ba171ee7a9891cf
Rachel <borapark13@gmail.com>
2022-05-23 23:42:24 +0900

    Modify: pug 설정 추가(line 6-7), 라우터 세팅(line 9), static작업(line8)

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello ");
-
-app.listen(3000);
+/**pug 설정 */
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening On http://localhost:3000`);
+app.listen(3000, handleListen);
