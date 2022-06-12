83cbe1ca0617419a18514b34b6c0b1798b1082b9
Rachel <borapark13@gmail.com>
2022-06-13 00:03:49 +0900

    Add: socket.io 설치 및 기본세팅

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
-import { parse } from "path";
-import WebSocket, { WebSocketServer } from "ws";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -12,26 +11,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
-const wses = [];
-
-wss.on("connection", (ws) => {
-  wses.push(ws);
-  ws["nickname"] = "Anonymous"; //nickname 초기값 설정
-  console.log("Connected to Browser");
-
-  ws.on("close", () => console.log("Disconnected From the Browser"));
-  ws.on("message", (message) => {
-    const msg = JSON.parse(message);
-    switch (msg.type) {
-      case "new_message":
-        wses.forEach((ws) => ws.send(`${ws.nickname} : ${msg.payload}`));
-      case "nickname":
-        ws["nickname"] = msg.payload; //웹소켓 안에 정보를 저장할 수 있다.
-    }
-  });
+io.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, () => console.log(`Listening On http://localhost:3000`));
+httpServer.listen(3000);
